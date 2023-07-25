4b4b5d30fb80ccd8199620aecfb1f7c3135715bc
anurag <anuragpathakoffl@gmail.com>
2023-07-25 12:42:18 +0530

    basic working model

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container-grid");
+
+createDivs(16);
 
 function createDivs(num) {
   for (let i = 1; i <= num; i++) {
     let gridRows = document.createElement("div");
     gridRows.classList.add("row");
     container.appendChild(gridRows);
+    //console.log(gridRows);
 
     for (let j = 1; j <= num; j++) {
       let gridColumn = document.createElement("div");
       gridColumn.classList.add("column");
       gridRows.appendChild(gridColumn);
+      //console.log(gridColumn);
     }
   }
 }
 
-createDivs(50);
+const divs = document.querySelectorAll(".column");
+//console.log(divs);
+divs.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    item.style.backgroundColor = "pink";
+  });
+});
