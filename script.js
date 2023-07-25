3a240620f15994423d48d1839be68842e0e3816b
anurag <anuragpathakoffl@gmail.com>
2023-07-25 17:35:16 +0530

    slider to change input

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.querySelector(".container-grid");
 
-createDivs(16);
-
+// create new grid
 function createDivs(num) {
   for (let i = 1; i <= num; i++) {
     let gridRows = document.createElement("div");
@@ -18,10 +17,29 @@ function createDivs(num) {
   }
 }
 
+// removes older grid
+function resetDivs() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+// each small box in a grid is divs
 const divs = document.querySelectorAll(".column");
 //console.log(divs);
 divs.forEach((item) => {
   item.addEventListener("mouseover", function () {
     item.style.backgroundColor = "pink";
   });
+});
+
+// Slider
+const mySlider = document.querySelector("#my-slider");
+const rangeValue = document.querySelector("#rangeValue");
+
+mySlider.addEventListener("input", function () {
+  rangeValue.innerHTML = mySlider.value;
+  let num = rangeValue.innerHTML;
+  resetDivs();
+  createDivs(num);
 });
